da4d964e6a14fe13d373bc3dcef0ecf75538b8f8
EmmanuelSW <edhgt89@hotmail.com>
2018-08-31 13:41:28 -0500

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  head: {
+    title: 'Nuxt EmmanuelSW',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  modules: [
+    '@nuxtjs/axios',
+  ],
+  loading: false,
+  build: {
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
---
+++ pages/users/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        Users
+      </h1>
+      <p>{{ip}}</p>
+      <nuxt-link to="/" class="button--green">Home</nuxt-link>
+    </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: ''
+    }
+  },
+  async asyncData({ app }) {
+    const ip = await app.$axios.$get('http://icanhazip.com')
+    return { ip: ip, message: ip }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  padding-top: 20px;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 40px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
